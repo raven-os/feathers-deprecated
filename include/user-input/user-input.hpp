ede97a8ff6feede537aad5b5c5309401dbcadbea
riamon_v <vincent.riamon@epitech.eu>
2018-05-15 19:35:31 +0200

    Start to handle input tutorial in POO

---
+++ include/user-input/user-input.hpp
@@ -0,0 +1,10 @@
+#pragma once;
+
+class userInput {
+public:
+  userInput();
+  ~userInput();
+
+private:
+
+};
---
+++ include/user-input/window.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <wayland-egl.h>
+#include <EGL/egl.h>
+#include <GL/gl.h>
+
+struct window {
+  EGLContext egl_context;
+  struct wl_surface *surface;
+  struct wl_shell_surface *shell_surface;
+  struct wl_egl_window *egl_window;
+  EGLSurface egl_surface;
+};
+
+class WindowHandler {
+public:
+  WindowHandler(EGLDisplay egl_display, struct wl_shell *shell,
+		struct wl_compositor *compositor, struct window *win);
+  ~WindowHandler();
+
+  void draw();
+  struct window const& getWindow() const;
+
+private:
+  static constexpr int WIDTH = 256;
+  static constexpr int HEIGHT = 256;
+  EGLDisplay& egl_display;
+  struct wl_shell *shell;
+  struct window *window;
+};
