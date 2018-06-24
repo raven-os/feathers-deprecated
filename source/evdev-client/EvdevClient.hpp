12e7728def3d870578cf776da49483e6396cf4a1
Sheep <louis.guittard@epitech.eu>
2018-06-24 19:24:56 +0200

    Added a function to create a new xkb keymap in the wrapper and used it to initialize the EvdevClient

--- source/evdev-client/EvdevClient.hpp
+++ source/evdev-client/EvdevClient.hpp
@@ -7,6 +7,7 @@
 enum toDestroy
 {
 	ALL = 0,
+	FROM_KEYMAP,
 	ONLY_CONTEXT
 };
 
--- source/evdev-client/XkbWrapper.cpp
+++ source/evdev-client/XkbWrapper.cpp
@@ -6,11 +6,15 @@ struct xkb_context *XkbWrapper::newContext() const
 	enum xkb_context_flags flags(XKB_CONTEXT_NO_FLAGS);
 
 	ctx = xkb_context_new(flags);
-	if (!ctx)
-	{
-		return NULL;
-	}
 	xkb_context_include_path_append(ctx, DATA_PATH);
+	return (ctx);
+}
 
-	return ctx;
+struct xkb_keymap *XkbWrapper::newKeymap(struct xkb_context *context) const
+{
+	struct xkb_keymap *keymap;
+	enum xkb_keymap_compile_flags flags(XKB_KEYMAP_COMPILE_NO_FLAGS);
+
+	keymap = xkb_keymap_new_from_names(context, NULL, flags);
+	return (keymap);
 }
