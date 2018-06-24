a4d134e15c9e319705eba4b94ca8e666057db4dc
Sheep <louis.guittard@epitech.eu>
2018-06-24 22:07:33 +0200

    Added a function to get the keyboards in the wrapper (<= need clean up) and used it in the initialisation of the EvdevClient

--- source/evdev-client/EvdevClient.cpp
+++ source/evdev-client/EvdevClient.cpp
@@ -25,25 +25,35 @@ bool EvdevClient::initClient()
 	ctx = xkbWrapper.newContext();
 	if (!ctx)
 	{
-		fprintf(stderr, "Couldn't create xkb context\n");
+		fprintf(stderr, "Couldn't create xkb context.\n");
 		return (valid);
 	}
 	destructionFlag = toDestroy::ONLY_CONTEXT;
 	keymap = xkbWrapper.newKeymap(ctx);
 	if (!keymap)
 	{
-		fprintf(stderr, "Couldn't create xkb keymap\n");
+		fprintf(stderr, "Couldn't create xkb keymap.\n");
 		return (valid);
 	}
 	destructionFlag = toDestroy::FROM_KEYMAP;
+	kbds = xkbWrapper.getKeyboards(keymap);
+	if (!kbds) {
+		fprintf(stderr, "Couldn't get any keyboard.\n");
+		return (valid);
+	}
+	destructionFlag = toDestroy::FROM_KEYBOARDS;
 	valid = true;
 	return (valid);
 }
 
 void EvdevClient::destroyClient()
 {
-	switch (destructionFlag) {
+	switch (destructionFlag)
+	{
 		case toDestroy::ALL:
+		case toDestroy::FROM_KEYBOARDS:
+		xkbWrapper.freeKeyboards(kbds);
+		[[fallthrough]];
 		case toDestroy::FROM_KEYMAP:
 		xkb_keymap_unref(keymap);
 		[[fallthrough]];
--- source/evdev-client/XkbWrapper.hpp
+++ source/evdev-client/XkbWrapper.hpp
@@ -1,11 +1,34 @@
 #pragma once
 
-#include "xkbcommon/xkbcommon.h"
+#include <dirent.h>
+#include <fnmatch.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <climits>
+#include <linux/input.h>
+#include <assert.h>
+#include <xkbcommon/xkbcommon.h>
+#include <iostream>
 
 #define DATA_PATH "./test/data"
+#define NLONGS(n) (((n) + (CHAR_BIT * sizeof(long)) - 1) / (CHAR_BIT * sizeof(long)))
+
+struct keyboard
+{
+    char *path;
+    int fd;
+    struct xkb_state *state;
+	struct xkb_compose_state *compose_state;
+    struct keyboard *next;
+};
 
 struct XkbWrapper
 {
 	struct xkb_context *newContext() const;
 	struct xkb_keymap *newKeymap(struct xkb_context *context) const;
+	struct keyboard *getKeyboards(struct xkb_keymap *keymap) const;
+	void freeKeyboards(struct keyboard *kbds) const;
 };
