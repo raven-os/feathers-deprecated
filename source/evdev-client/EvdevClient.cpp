eebfee60acf5c9eceb310153c308df8f37daa2df
Sheep <louis.guittard@epitech.eu>
2018-06-24 23:27:41 +0200

    Added a function to loop on the input reading (<= need a LOT of cleaning)

--- source/evdev-client/EvdevClient.cpp
+++ source/evdev-client/EvdevClient.cpp
@@ -8,6 +8,7 @@ layout(NULL),
 variant(NULL),
 options(NULL),
 keymap_path(NULL),
+terminate(NULL),
 valid(false)
 {
     std::cout << "EvdevClient constructed." << std::endl;
@@ -281,4 +282,191 @@ void EvdevClient::destroyClient()
 bool EvdevClient::isValid() const
 {
 	return (valid);
+}
+
+/* The meaning of the input_event 'value' field. */
+enum {
+	KEY_STATE_RELEASE = 0,
+	KEY_STATE_PRESS = 1,
+	KEY_STATE_REPEAT = 2,
+};
+
+void test_print_keycode_state(struct xkb_state *state,
+	xkb_keycode_t keycode,
+	enum xkb_consumed_mode consumed_mode
+)
+{
+	struct xkb_keymap *keymap;
+	const xkb_keysym_t *syms;
+	int nsyms;
+	char s[16];
+	xkb_layout_index_t layout;
+
+	keymap = xkb_state_get_keymap(state);
+	nsyms = xkb_state_key_get_syms(state, keycode, &syms);
+	if (nsyms <= 0)
+	{
+		return;
+	}
+	printf("\nkeysyms [ ");
+	for (int i = 0; i < nsyms; i++)
+	{
+		xkb_keysym_get_name(syms[i], s, sizeof(s));
+		printf("%-*s ", (int) sizeof(s), s);
+	}
+	printf("] ");
+	xkb_state_key_get_utf8(state, keycode, s, sizeof(s));
+	printf("unicode [ %s ] ", s);
+	layout = xkb_state_key_get_layout(state, keycode);
+	printf("layout [ %s (%d) ] ",
+	xkb_keymap_layout_get_name(keymap, layout), layout);
+	printf("level [ %d ] ",
+	xkb_state_key_get_level(state, keycode, layout));
+	printf("mods [ ");
+	for (xkb_mod_index_t mod = 0; mod < xkb_keymap_num_mods(keymap); mod++)
+	{
+		if (xkb_state_mod_index_is_active(state, mod, XKB_STATE_MODS_EFFECTIVE) <= 0)
+		{
+			continue;
+		}
+		if (xkb_state_mod_index_is_consumed2(state, keycode, mod, consumed_mode))
+		{
+			printf("-%s ", xkb_keymap_mod_get_name(keymap, mod));
+		}
+		else
+		{
+			printf("%s ", xkb_keymap_mod_get_name(keymap, mod));
+		}
+	}
+	printf("] ");
+	printf("leds [ ");
+	for (xkb_led_index_t led = 0; led < xkb_keymap_num_leds(keymap); led++)
+	{
+		if (xkb_state_led_index_is_active(state, led) <= 0)
+		{
+			continue;
+		}
+		printf("%s ", xkb_keymap_led_get_name(keymap, led));
+	}
+	printf("] ");
+	printf("\n");
+}
+
+void EvdevClient::process_event(struct keyboard *kbd,
+	uint16_t type,
+	uint16_t code,
+	int32_t value
+)
+{
+	xkb_keycode_t keycode;
+	struct xkb_keymap *keymap;
+
+	if (type != EV_KEY)
+	{
+		return;
+	}
+	keycode = EVDEV_OFFSET + code;
+	if (keycode == 9)
+	{
+		terminate = true;
+	}
+	keymap = xkb_state_get_keymap(kbd->state);
+	if (value == KEY_STATE_REPEAT && !xkb_keymap_key_repeats(keymap, keycode))
+	{
+		return;
+	}
+	if (value != KEY_STATE_RELEASE)
+	{
+		test_print_keycode_state(kbd->state,
+			keycode,
+			XKB_CONSUMED_MODE_XKB
+		);
+	}
+	if (value == KEY_STATE_RELEASE)
+	{
+		xkb_state_update_key(kbd->state, keycode, XKB_KEY_UP);
+	}
+	else
+	{
+		xkb_state_update_key(kbd->state, keycode, XKB_KEY_DOWN);
+	}
+}
+
+int EvdevClient::read_keyboard(struct keyboard *kbd)
+{
+	ssize_t len;
+	struct input_event evs[16];
+
+	while ((len = read(kbd->fd, &evs, sizeof(evs))) > 0)
+	{
+		const size_t nevs = len / sizeof(struct input_event);
+		for (size_t i = 0; i < nevs; i++)
+		{
+			process_event(kbd, evs[i].type, evs[i].code, evs[i].value);
+		}
+	}
+	if (len < 0 && errno != EWOULDBLOCK)
+	{
+		fprintf(stderr, "Couldn't read %s: %s\n", kbd->path, strerror(errno));
+		return -errno;
+	}
+	return 0;
+}
+
+int EvdevClient::loop()
+{
+	int i, ret;
+	int epfd;
+	struct keyboard *kbd;
+	struct epoll_event ev;
+	struct epoll_event evs[16];
+
+	epfd = epoll_create1(0);
+	if (epfd < 0)
+	{
+		fprintf(stderr, "Couldn't create epoll instance: %s\n",
+		strerror(errno));
+		return -errno;
+	}
+	for (kbd = kbds; kbd; kbd = kbd->next)
+	{
+		memset(&ev, 0, sizeof(ev));
+		ev.events = EPOLLIN;
+		ev.data.ptr = kbd;
+		ret = epoll_ctl(epfd, EPOLL_CTL_ADD, kbd->fd, &ev);
+		if (ret)
+		{
+			ret = -errno;
+			fprintf(stderr, "Couldn't add %s to epoll: %s\n",
+			kbd->path, strerror(errno));
+			goto err_epoll;
+		}
+	}
+	while (!terminate)
+	{
+		ret = epoll_wait(epfd, evs, 16, -1);
+		if (ret < 0)
+		{
+			if (errno == EINTR)
+			continue;
+			ret = -errno;
+			fprintf(stderr, "Couldn't poll for events: %s\n",
+			strerror(errno));
+			goto err_epoll;
+		}
+		for (i = 0; i < ret; i++)
+		{
+			kbd = (struct keyboard *)evs[i].data.ptr;
+			ret = read_keyboard(kbd);
+			if (ret)
+			{
+				goto err_epoll;
+			}
+		}
+	}
+	close(epfd);
+	return 0;
+	err_epoll:
+	close(epfd);
+	return ret;
 }
