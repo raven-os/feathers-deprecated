0f514302e532ab0889a2540b4f29e0f642e81c35
Sheep <louis.guittard@epitech.eu>
2018-06-24 18:44:27 +0200

    Added a function to create a new xkb context in the wrapper

--- source/evdev-client/XkbWrapper.cpp
+++ source/evdev-client/XkbWrapper.cpp
@@ -1,1 +1,15 @@
 #include "XkbWrapper.hpp"
+
+struct xkb_context *newContext() const
+{
+	struct xkb_context *ctx;
+
+	ctx = xkb_context_new(0);
+	if (!ctx)
+	{
+		return NULL;
+	}
+	xkb_context_include_path_append(ctx, DATA_PATH);
+
+	return ctx;
+}
