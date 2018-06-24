4eee8a63c8196475a9cfec7fbb556c90d105f2d6
Sheep <louis.guittard@epitech.eu>
2018-06-24 17:47:31 +0200

    added the needed variables as attributes to EvdevClient

---
+++ source/evdev-client/EvdevClient.cpp
@@ -0,0 +1,11 @@
+#include "EvdevClient.hpp"
+
+EvdevClient::EvdevClient()
+{
+	std::cout << "EvdevClient constructed." << std::endl;
+}
+
+EvdevClient::~EvdevClient()
+{
+	std::cout << "EvdevClient destructed." << std::endl;
+}
