6099f4923d3267bd4f6f81fd43ed925548410fc1
Sheep <louis.guittard@epitech.eu>
2018-06-24 17:52:16 +0200

    initializing pointers as NULL

--- source/evdev-client/EvdevClient.cpp
+++ source/evdev-client/EvdevClient.cpp
@@ -1,6 +1,15 @@
 #include "EvdevClient.hpp"
 
 EvdevClient::EvdevClient()
+:
+compose_table(NULL),
+rules(NULL),
+model(NULL),
+layout(NULL),
+variant(NULL),
+options(NULL),
+keymap_path(NULL),
+isValid(false)
 {
 	std::cout << "EvdevClient constructed." << std::endl;
 }
