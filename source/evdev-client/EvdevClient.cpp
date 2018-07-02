6b942deba0bca975856a7c663c83a32258279c40
Sheep <louis.guittard@epitech.eu>
2018-07-02 16:58:52 +0200

    cleanup on read_keyboard and process_event

--- source/evdev-client/EvdevClient.cpp
+++ source/evdev-client/EvdevClient.cpp
@@ -57,8 +57,7 @@ bool EvdevClient::initClient()
         ev.data.ptr = kbd;
         if (epoll_ctl(epfd, EPOLL_CTL_ADD, kbd->fd, &ev))
         {
-            fprintf(stderr, "Couldn't add %s to epoll: %s\n",
-            kbd->path, strerror(errno));
+            fprintf(stderr, "Couldn't add %s to epoll: %s\n", kbd->path, strerror(errno));
             return (valid);
         }
     }
@@ -82,6 +81,8 @@ void EvdevClient::destroyClient()
         [[fallthrough]];
         case toDestroy::ONLY_CONTEXT:
         xkb_context_unref(ctx);
+        default:
+        break;
     }
 }
 
@@ -171,11 +172,6 @@ void EvdevClient::process_event(struct keyboard *kbd,
     {
         return;
     }
-    keycode = EVDEV_OFFSET + code;
-    if (keycode == 9)
-    {
-        terminate = true;
-    }
     keymap = xkb_state_get_keymap(kbd->state);
     if (value == KEY_STATE_REPEAT && !xkb_keymap_key_repeats(keymap, keycode))
     {
@@ -206,6 +202,7 @@ int EvdevClient::read_keyboard(struct keyboard *kbd)
     while ((len = read(kbd->fd, &evs, sizeof(evs))) > 0)
     {
         const size_t nevs = len / sizeof(struct input_event);
+
         for (size_t i = 0; i < nevs; i++)
         {
             process_event(kbd, evs[i].type, evs[i].code, evs[i].value);
@@ -214,9 +211,9 @@ int EvdevClient::read_keyboard(struct keyboard *kbd)
     if (len < 0 && errno != EWOULDBLOCK)
     {
         fprintf(stderr, "Couldn't read %s: %s\n", kbd->path, strerror(errno));
-        return -errno;
+        return (1);
     }
-    return 0;
+    return (0);
 }
 
 void EvdevClient::tick()
