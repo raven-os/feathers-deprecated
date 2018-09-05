d27a24b8e3a3c0e1a17e3bc36f1eceedeb3b66e7
riamon_v <vincent.riamon@epitech.eu>
2018-09-05 10:53:33 +0200

    Added wayland_client namespace in header listeners files

--- include/listeners/KeyboardListener.hpp
+++ include/listeners/KeyboardListener.hpp
@@ -6,23 +6,26 @@
 #include <unistd.h>
 #include <iostream>
 
-class KeyboardListener
+namespace wayland_client
 {
-  struct xkb_context *xkbContext;
-  struct xkb_keymap *keymap{nullptr};
-  struct xkb_state *xkbState{nullptr};
+  class KeyboardListener
+  {
+    struct xkb_context *xkbContext;
+    struct xkb_keymap *keymap{nullptr};
+    struct xkb_state *xkbState{nullptr};
 
-  bool running = true;
+    bool running = true;
 
-  public:
-    KeyboardListener();
-    ~KeyboardListener() = default;
+    public:
+      KeyboardListener();
+      ~KeyboardListener() = default;
 
-    void keyboardKeymap(struct wl_keyboard *keyboard, uint32_t format, int32_t fd, uint32_t size);
-    void keyboardEnter(struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface, struct wl_array *keys);
-    void keyboardLeave(struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface);
-    void keyboardKey(struct wl_keyboard *keyboard, uint32_t serial, uint32_t time, uint32_t key, uint32_t state);
-    void keyboardModifiers(struct wl_keyboard *keyboard, uint32_t serial, uint32_t modsDepressed, uint32_t modsLatched, uint32_t modsLocked, uint32_t group);
+      void keyboardKeymap(struct wl_keyboard *keyboard, uint32_t format, int32_t fd, uint32_t size);
+      void keyboardEnter(struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface, struct wl_array *keys);
+      void keyboardLeave(struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface);
+      void keyboardKey(struct wl_keyboard *keyboard, uint32_t serial, uint32_t time, uint32_t key, uint32_t state);
+      void keyboardModifiers(struct wl_keyboard *keyboard, uint32_t serial, uint32_t modsDepressed, uint32_t modsLatched, uint32_t modsLocked, uint32_t group);
 
-    bool getRunning() const;
-};
+      bool getRunning() const;
+  };
+}
--- include/listeners/SeatListener.hpp
+++ include/listeners/SeatListener.hpp
@@ -5,17 +5,20 @@
 #include "listeners/KeyboardListener.hpp"
 #include "listeners/PointerListener.hpp"
 
-class SeatListener {
+namespace wayland_client
+{
+  class SeatListener {
 
-  KeyboardListener *keyboardListener;
-  PointerListener *pointerListener;
+    KeyboardListener *keyboardListener;
+    PointerListener *pointerListener;
 
-  public:
-    SeatListener();
-    ~SeatListener() = default;
+    public:
+      SeatListener();
+      ~SeatListener() = default;
 
-    void seatCapabilities(struct wl_seat *seat, uint32_t capabilities);
-    void seatName(struct wl_seat *seat, const char *name);
+      void seatCapabilities(struct wl_seat *seat, uint32_t capabilities);
+      void seatName(struct wl_seat *seat, const char *name);
 
-    bool getRunning() const;
-};
+      bool getRunning() const;
+  };
+}
